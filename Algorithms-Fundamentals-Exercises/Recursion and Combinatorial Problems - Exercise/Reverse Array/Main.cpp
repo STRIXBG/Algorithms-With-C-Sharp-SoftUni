398757949f93df9d3c408fe8e37978bd907b8768
STRIXBG <65187330+STRIXBG@users.noreply.github.com>
2022-11-07 21:39:57 +0200

    Update Main.cpp

--- Algorithms-Fundamentals-Exercises/Recursion and Combinatorial Problems - Exercise/Reverse Array/Main.cpp
+++ Algorithms-Fundamentals-Exercises/Recursion and Combinatorial Problems - Exercise/Reverse Array/Main.cpp
@@ -1,3 +1,11 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ConsoleApp1
 {
     internal class Program
